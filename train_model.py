4ea01d1af7496e84fad9a07360d29b09e4c4fafa
Long (Leo) Nguyen <59188080+LongNguyen247@users.noreply.github.com>
2025-08-07 20:39:41 +0700

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -18,7 +18,7 @@ X_train, X_test, y_train, y_test = train_test_split(text_data, labels, test_size
 
 # Vectorizer
 vectorizer = TfidfVectorizer(stop_words="english", max_features=5000)
-X_train_vec = vectorizer.fit_transform(X_train)
+X_train_vec = vectorizer.fit_transform(X_train)   
 X_test_vec = vectorizer.transform(X_test)
 
 # Model
